dba4e91b6e0ed6cd84f666d25da7dba773796a6b
codexuttam <uttamrajsingh423@gmail.com>
2026-04-26 20:58:16 +0530

    Fix CI build by marking API routes as force-dynamic

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 import { generateOutreach } from '../../../lib/openai';
 import connect from '../../../lib/db';
 import Lead from '../../../models/Lead';
--- app/api/scrape/route.js
+++ app/api/scrape/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../auth/[...nextauth]/route"
 import { scrapeBusinesses } from '../../../lib/scraper';
