89f2a3f2feafca79ccfd8e118c501de249a73ec8
codexuttam <uttamrajsingh423@gmail.com>
2026-06-16 16:18:46 +0530

    feat: add logger utility

---
+++ utils/logger.js
@@ -0,0 +1,5 @@
+export const logger = {
+  info: (msg, ...args) => console.log(`[INFO] ${msg}`, ...args),
+  error: (msg, ...args) => console.error(`[ERROR] ${msg}`, ...args),
+  warn: (msg, ...args) => console.warn(`[WARN] ${msg}`, ...args),
+};
