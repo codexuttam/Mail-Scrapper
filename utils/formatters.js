2d296cb3a88a157310a2e412f6b90661066fc4e5
codexuttam <uttamrajsingh423@gmail.com>
2026-06-16 16:19:06 +0530

    feat: add formatters utility

---
+++ utils/formatters.js
@@ -0,0 +1,13 @@
+export function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(new Date(date));
+}
+
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+}
